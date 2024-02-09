94d1080b6913987d6e277fbb7f4c1a351b38c6ea
Mete <metegonel@hotmail.com>
2024-02-09 17:18:30 -0500

    initial commit

---
+++ app/src/main/java/com/example/shoppinglist/ShoppingActivity.kt
@@ -0,0 +1,48 @@
+package com.example.shoppinglist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.shoppinglist.data.db.ShoppingDatabase
+import com.example.shoppinglist.data.db.entities.ShoppingItem
+import com.example.shoppinglist.data.repositories.ShoppingRepository
+import com.example.shoppinglist.other.ShoppingItemAdapter
+import com.example.shoppinglist.ui.shoppinglist.AddDialogListener
+import com.example.shoppinglist.ui.shoppinglist.AddShoppingItemDialog
+import com.example.shoppinglist.ui.shoppinglist.ShoppingViewModel
+import com.example.shoppinglist.ui.shoppinglist.ShoppingViewModelFactory
+import kotlinx.android.synthetic.main.activity_shopping.*
+
+class ShoppingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping)
+
+        val database = ShoppingDatabase(this)
+        val repository = ShoppingRepository(database)
+        val factory = ShoppingViewModelFactory(repository)
+
+        val viewModel = ViewModelProviders.of(this, factory).get(ShoppingViewModel::class.java)
+
+        val adapter = ShoppingItemAdapter(listOf(), viewModel)
+
+        rvShoppingItems.layoutManager = LinearLayoutManager(this)
+        rvShoppingItems.adapter = adapter
+
+        viewModel.getAllShoppingItems().observe(this, Observer {
+            adapter.items = it
+            adapter.notifyDataSetChanged()
+        })
+
+        fab.setOnClickListener {
+            AddShoppingItemDialog(this,
+            object : AddDialogListener {
+                override fun onAddButtonClicked(item: ShoppingItem) {
+                    viewModel.upsert(item)
+                }
+            }).show()
+        }
+    }
+}
